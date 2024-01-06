0b3dda43481b287e67595ca602628ad08bf6c93f
CHUKWUDI INNOCENT <125125776+Innochu@users.noreply.github.com>
2024-01-06 20:30:54 +0100

    worked on user and cartitem model as well as validations

---
+++ BookHaven.Domain/Entities/Book.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookHaven.Domain.Entities
+{
+    public class Book
+    {
+        public long Id { get; set; }
+
+        [Required (ErrorMessage ="Enter a valid Title")]
+        public string Title { get; set; } = string.Empty;
+
+        [Required, MinLength(4, ErrorMessage = "minimum lenght is 20")]
+        public string ShortDescription { get; set; } = string.Empty;
+
+        [Required]
+        public string Author { get; set; } = string.Empty;
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage ="please enter a value")]
+        [Column(TypeName = "decimal(8, 2)")]
+        public decimal Price { get; set; }
+
+        public long CategoryId { get; set; }
+        public ICollection<Category> Category { get; set; }
+    }
+}
---
+++ BookHaven.Domain/Entities/Cart.cs
@@ -0,0 +1,8 @@
+﻿namespace BookHaven.Domain.Entities
+{
+    public class Cart
+    {
+        public List<CartItem> CartItems { get; set; } = new List<CartItem>();
+        public long GrandTotal { get; set; }
+    }
+}
